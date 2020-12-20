2e011ed513038f1493e577cc257e6ed68e5ba08e
ArlonTuazon <nikka1378@yahoo.com>
2020-12-20 01:17:03 -0800

    added intern inquirer

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const promptEngineer = () => {
     {
         type: "input",
         name: "name",
-        message: "Enter Engineer's name?",
+        message: "Enter Engineer's name:",
         validate: nameEngineer=> {
             if (nameEngineer) {
                 return true;
@@ -24,20 +24,19 @@ const promptEngineer = () => {
     {
         type: "input",
         name: "id",
-        message: "Enter Engineer's ID?",
+        message: "Enter Engineer's ID:",
         validate: idEngineer => {
             if (isNaN(idEngineer)) {
                 return "please enter a number";
               }
               return true;
             }
-            
     }, 
     
     {
         type: "input",
         name: "email",
-        message: "Enter Engineer's email?",
+        message: "Enter Engineer's email:",
         validate: emailEngineer=> {
             if (emailEngineer) {
                 return true;
@@ -47,10 +46,90 @@ const promptEngineer = () => {
                 return false;
             }
         }
-            
+    }, 
+
+    {
+        type: "input",
+        name: "github",
+        message: "Enter Engineer's Github:",
+    }, 
+
+    {
+        type: "list",
+        name: "addMore",
+        message: "Would you like to add another Engineer or Intern?",
+        choices: ["Engineer", "Intern", "None"]
     }, 
 ])
 
+}
+const promptIntern = () => {
+    return inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "Enter Intern's name:",
+        validate: nameIntern=> {
+            if (nameIntern) {
+                return true;
+            }
+            else {
+                console.log ('Please Enter Name!')
+                return false;
+            }
+        }
+    },
+
+    {
+        type: "input",
+        name: "id",
+        message: "Enter Intern's ID:",
+        validate: idIntern => {
+            if (isNaN(idIntern)) {
+                return "please enter a number";
+              }
+              return true;
+            }
+    }, 
+
+    {
+        type: "input",
+        name: "email",
+        message: "Enter Intern's email:",
+        validate: emailIntern => {
+            if (emailIntern) {
+                return true;
+            }
+            else {
+                console.log ('Please Enter email!')
+                return false;
+            }
+        }
+    },
+
+    {
+        type: "input",
+        name: "school",
+        message: "Enter Intern's school:",
+        validate: schoolIntern=> {
+            if (schoolIntern) {
+                return true;
+            }
+            else {
+                console.log ('Please Enter School!')
+                return false;
+            }
+        }
+    },
+    
+    {
+        type: "list",
+        name: "addMore",
+        message: "Would you like to add another Engineer or Intern?",
+        choices: ["Engineer", "Intern", "None"]
+    }, 
+
+])
 }
 promptEngineer().then(function (answers){
     console.log(answers);
@@ -63,3 +142,13 @@ promptEngineer().then(function (answers){
     }
   })
 
+  promptIntern().then(function (answers){
+    console.log(answers);
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  })
