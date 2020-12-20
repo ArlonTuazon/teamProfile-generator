8f20db8d42f8bbb975b2309865c9ffa166115b75
ArlonTuazon <nikka1378@yahoo.com>
2020-12-19 22:33:16 -0800

    run getrole in intern test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,7 +1,13 @@
 const Intern = require ('../lib/Intern.js')
 
-test ('Sets Intern School getSchool()', () =>{
+test ('Sets Intern School getSchool()', () => {
     const school = 'UCLA';
     const intern = new Intern ('Arrielle', 2, 'arrielle@test.com', school);
     expect (intern.getSchool()).toBe(school);
+});
+
+test ('"getRole() should return \"Intern\""', () => {
+    const tempVal = 'Intern';
+    const intern = new Intern ('Arrielle', 2, 'arrielle@test.com', 'UCLA')
+    expect (intern.getRole()).toBe(tempVal);
 });
