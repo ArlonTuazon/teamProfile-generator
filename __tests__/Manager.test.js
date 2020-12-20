fb79e3f564af11eb33afeafed1abd995e2300251
ArlonTuazon <nikka1378@yahoo.com>
2020-12-19 22:51:12 -0800

    pass the Manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,8 +1,14 @@
 const Manager = require ('../lib/Manager.js');
 //const Employee = require ('../lib/Employee.js')
 
-test ('Gets office number via getOfficeNum()', () =>{
+test('Gets office number via getOfficeNum()', () =>{
    const officeNum = 88888 
    const manager = new Manager('Nikka', 3, 'nikka@test.com', officeNum);
    expect(manager.getOfficeNum()).toBe(officeNum); 
+});
+
+test('getRole() should return "Manager"',() => {
+    const tempVal = 'Manager';
+    const manager = new Manager('Nikka', 3, 'nikka@test.com', 88888);
+    expect(manager.getRole()).toBe(tempVal);
 });
