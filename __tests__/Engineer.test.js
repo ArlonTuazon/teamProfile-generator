bbed023f1b5db7aec2df636b98e1f7ea3a8fea68
ArlonTuazon <nikka1378@yahoo.com>
2020-12-19 22:20:27 -0800

    passed all engineer codes

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
-const Employee = require ('../lib/Employee.js')
+const Engineer = require ('../lib/Engineer.js');
+
+test("Sets GitHUb account", () => {
+    const tempVal = 'GitUserJay';
+    const engineer = new Engineer('Jay', 1, 'jay@yahoo.com', tempVal);
+    expect(engineer.github).toBe(tempVal);
+  });
+  
+  test("getRole() should return \"Engineer\"", () => {
+    const tempVal = "Engineer";
+    const engineer = new Engineer('Jay', 1, 'jay@yahoo.com', 'GitUserJay');
+    expect(engineer.getRole()).toBe(tempVal);
+  });
+  
+  test("Can get GitHub username via getGithub()", () => {
+    const github = 'GitUserJay'
+    const engineer = new Engineer('Jay', 1, 'jay@yahoo.com', github);
+    expect(engineer.getgitHub()).toBe(github);
+  });
