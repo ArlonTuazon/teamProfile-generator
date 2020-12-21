01752369a2d6d77400eb0ad5f308530378ff7b0d
ArlonTuazon <nikka1378@yahoo.com>
2020-12-20 22:06:00 -0800

    finished page template

--- src/page-template.js
+++ src/page-template.js
@@ -12,4 +12,51 @@ const teamCards = teamData => {
         `
        return managerHTML
     });
+
+    const engineer = teamData.engineer.map (function (job){
+        let engineerHTML = `
+        <div class = "card" style="width: 18rem:">
+            <h2>${job.name}</h2>
+            <h4>Engineer</h4>
+            <p>ID: ${job.id}</p>
+            <p>Email: <a href = "mailto:${job.email}">${job.email}</a></p>
+            <p>Github: <a href = "https://github.com/${job.github}" target="_blank">${job.github}</a></p>
+        </div>
+        `
+       return engineerHTML
+    });
+
+    const intern = teamData.intern.map (function (job){
+        let internHTML = `
+        <div class = "card" style="width: 18rem:">
+            <h2>${job.name}</h2>
+            <h4>Intern</h4>
+            <p>ID: ${job.id}</p>
+            <p>Email: <a href = "mailto:${job.email}">${job.email}</a></p>
+            <p>School: ${job.school}</p>
+        </div>
+        `
+       return internHTML
+    });
+    return [manager,engineer,intern]
+}
+
+module.exports = employeeData => {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+            <title>My Team</title>
+        </head>
+        <body>
+            <header>
+            <h1 class="text-center">My Team</h1>
+            </header>
+            ${teamCards(employeeData)}
+        </body>
+        </html>    
+        `
 }
